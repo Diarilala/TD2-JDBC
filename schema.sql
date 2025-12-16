5bd2c8e31db8ed25c2c39d847d0f2466a40c9260
Diarilala <hei.diarilala@gmail.com>
2025-12-16 13:59:02 +0300

    feat: add schema.sql

---
+++ schema.sql
@@ -0,0 +1,16 @@
+CREATE TYPE type AS ENUM('START','MAIN','DESSERT');
+CREATE TYPE category AS ENUM('VEGETABLE','ANIMAL','MARINE','DAIRY','OTHER');
+
+CREATE TABLE Dish(
+    id int PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    dish_type type NOT NULL,
+);
+
+CREATE TABLE Ingredient(
+    id int PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    price NUMERIC(10,2) NOT NULL,
+    category cateogory NOT NULL,
+    id_dish int REFERENCES Dish(id);
+)
